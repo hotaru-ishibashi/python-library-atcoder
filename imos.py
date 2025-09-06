5d34d571551fc564543df3f30f87561226e762f2
hotaru <hotaru@MyComputer.localdomain>
2025-09-07 00:10:53 +0900

    二次元imosに矩形和を追加

--- imos.py
+++ imos.py
@@ -22,6 +22,7 @@ class IMOS:
     
 
 class IMOS2D:
+    csum = None
     def __init__(self, H, W):
         self.grid = [[0] * (W+1) for _ in range(H+1)]
         self.H = H
@@ -38,16 +39,31 @@ class IMOS2D:
     def calc(self):
         """ 区間加算結果を計算。O(H*W)
         """
-        res = [[0] * self.W for _ in range(self.H)]
+        res = [[0] * (self.W + 1) for _ in range(self.H + 1)]
         for i in range(self.H):
             csum = 0
             for j in range(self.W):
                 csum += self.grid[i][j]
-                res[i][j] = csum
-        for j in range(self.W):
-            for i in range(1, self.H):
+                res[i+1][j+1] = csum
+        for j in range(self.W+1):
+            for i in range(1, self.H+1):
                 res[i][j] += res[i-1][j]
+        self.csum = res
         return res
+    
+    """ 矩形範囲内 [top, bottom), [left, right)] の合計を計算 O(1)
+        事前にimos.calc()が必要
+    """
+    def rect_sum(self, top, left, bottom, right):
+        if not self.csum:
+            raise ValueError("calc first")
+        # print(top, left, bottom, right)
+        m = self.csum[bottom][right]
+        t = self.csum[top][right]
+        l = self.csum[bottom][left]
+        tl = self.csum[top][left]
+    
+        return m - t - l + tl            
     
 
 class IMOS3D:
