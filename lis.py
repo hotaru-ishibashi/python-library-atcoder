33681bddade8795e987ad80ee2cf019f08af9ab6
hiromasa-hayashi <hiromasa-hayashi@netforce.co.jp>
2025-09-19 10:38:17 +0900

    LISの復元

--- lis.py
+++ lis.py
@@ -6,14 +6,33 @@ def LIS(A):
 
     dp = [inf] * (N+1)
     dp[0] = 0
+    # A[i]が増加列の何番目か
+    idx_memo = [None] * N
 
-    for val in A:
+    for i, val in enumerate(A):
         update_idx = bisect_left(dp, val)
         dp[update_idx] = val
+        idx_memo[i] = update_idx
 
-    lis = bisect_left(dp, inf) - 1
+    lis_len = bisect_left(dp, inf) - 1
+    return dp, lis_len, idx_memo
 
-    return dp, lis
+def reconstruct(A, lis_len, idx_memo):
+    N = len(A)
 
-a = [2, 6, 4, 0, 16, 8, 2]
-print(LIS(a))
+    t_idx = lis_len
+    hist = []
+    for i in range(N-1, -1, -1):
+        if idx_memo[i] == t_idx:
+            hist.append((A[i], i))
+            t_idx -= 1
+
+    hist.reverse()
+    return hist
+
+
+a = [5, 6, 7, 1, 2, 3, 4]
+dp, lis_len, idx_memo = LIS(a)
+lis = reconstruct(a, lis_len, idx_memo)
+
+print(lis)
