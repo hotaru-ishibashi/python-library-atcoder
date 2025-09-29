55a235f0746581fdb2ba05a276999f16d9290c4d
hiromasa-hayashi <hiromasa-hayashi@netforce.co.jp>
2025-09-29 10:32:16 +0900

    合成数modの組み合わせ

--- mod_cmb.py
+++ mod_cmb.py
@@ -11,3 +11,20 @@ class Modcomb:
         if n > self.nmax or k > self.nmax:
             raise ValueError("over maximum n/k")
         return self.cmb_memo[n] * pow(self.cmb_memo[n-k], -1, self.mod) * pow(self.cmb_memo[k], -1, self.mod) % self.mod
+    
+
+class CompositeModComb:
+    def __init__(self, nmax, mod):
+        dp = [[0] * (nmax + 1) for _ in range(nmax + 1)]
+        dp[1][1] = 1
+        for i in range(nmax + 1):
+            dp[i][0] = 1
+        for n in range(2, nmax + 1):
+            for r in range(1, n+1):
+                dp[n][r] = dp[n-1][r-1] + dp[n-1][r]
+                dp[n][r] %= mod
+
+        self.memo = dp
+
+    def comb(self, n, r):
+        return self.memo[n][r]
