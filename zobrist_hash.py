0d25aaacba1ba28a0ffe467d0b1c3d02cc413cc3
hiromasa-hayashi <hiromasa-hayashi@netforce.co.jp>
2025-11-19 14:14:18 +0900

    区間集合ハッシュ

---
+++ zobrist_hash.py
@@ -0,0 +1,20 @@
+from typing import List, Dict
+from random import randrange
+
+class RangeSetHash:
+    def __init__(self, V: List[int], table: Dict[int, int]):
+        self.csum = [0]
+        for e in V:
+            if not e in table:
+                table[e] = randrange(1<<60)
+        # seen = set()
+        for e in V:
+            self.csum.append(self.csum[-1] + table[e])    
+
+    def hash(self, l, r):
+        return self.csum[r] - self.csum[l]
+    
+table = {}
+rsh = RangeSetHash([1, 2, 3, 2, 3], table)
+
+print(rsh.hash(1, 3) == rsh.hash(3, 5))
