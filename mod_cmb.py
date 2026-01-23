c11449a6b8d70b983a6ba7d57414bd679f7c666e
hiromasa-hayashi <hiromasa-hayashi@netforce.co.jp>
2026-01-23 10:31:55 +0900

    逆元の前計算　重複組み合わせ

--- mod_cmb.py
+++ mod_cmb.py
@@ -1,17 +1,20 @@
-class Modcomb:
-    def __init__(self, nmax, mod=998244353):
-        self.cmb_memo = [1]
-        self.nmax = nmax
-        self.mod = mod
-        for i in range(1, nmax+1):
-            self.cmb_memo.append(self.cmb_memo[-1] * i % self.mod)
-    def comb(self, n, k):
-        if k > n:
-            return 0
-        if n > self.nmax or k > self.nmax:
-            raise ValueError("over maximum n/k")
-        return self.cmb_memo[n] * pow(self.cmb_memo[n-k], -1, self.mod) * pow(self.cmb_memo[k], -1, self.mod) % self.mod
-    
+
+""" @@@ EDIT @@@ """
+nmax = 3010
+mod = 998244353
+""" @@@@@@@@@@@@ """
+fac = [0] * (nmax+1)
+ifac = [0] * (nmax+1)
+fac[0] = ifac[0] = 1
+for i in range(1, nmax+1): fac[i] = fac[i-1]*i%mod
+ifac[nmax] = pow(fac[nmax], mod-2, mod)
+for i in range(nmax-1, 0, -1): ifac[i] = ifac[i+1]*(i+1)%mod
+def C(n, k):
+    if k > n: return 0
+    return fac[n] * ifac[n-k] * ifac[k] % mod
+def H(n, k):
+    if k > n: return 0
+    return C(n+k-1, k)
 
 class CompositeModComb:
     def __init__(self, nmax, mod):
