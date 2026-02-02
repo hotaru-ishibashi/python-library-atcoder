e5dde49bc2ab04947ef4a80c315dda6f6f66749e
hiromasa-hayashi <hiromasa-hayashi@netforce.co.jp>
2026-02-02 15:43:04 +0900

    始点を集合で渡せるように変更

--- tsp.py
+++ tsp.py
@@ -1,7 +1,14 @@
-def tsp(G, s):
+"""
+Docstring for tsp
+
+G: グラフ(隣接行列)
+S: 始点の集合
+"""
+def tsp(G, S=[0]):
     V = len(G)
     dp = [[float('inf')]*V for _ in range(2**V)] 
-    dp[1<<s][s] = 0 
+    for s in S:
+        dp[1<<s][s] = 0 
     for bit in range(2**V): 
         for u in range(V):
             if bit & (1<<u) == 0:
