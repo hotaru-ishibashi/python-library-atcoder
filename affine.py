56a494cca1add954097f4a5d7f5b328ea2cd9333
hiromasa-hayashi <hiromasa-hayashi@netforce.co.jp>
2026-04-17 09:58:58 +0900

    変換行列を一次元化

--- affine.py
+++ affine.py
@@ -1,10 +1,11 @@
 class Affine:
+    affine = [1, 0, 0, 0, 1, 0, 0, 0, 1]
     def __init__(self):
-        self.affine = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+        pass
 
     def apply(self, x, y):
-        x_ = x * self.affine[0][0] + y * self.affine[1][0] + self.affine[2][0]
-        y_ = x * self.affine[0][1] + y * self.affine[1][1] + self.affine[2][1]
+        x_ = x * self.affine[0] + y * self.affine[3] + self.affine[6]
+        y_ = x * self.affine[1] + y * self.affine[4] + self.affine[7]
         return x_, y_
 
     """
@@ -13,9 +14,9 @@ class Affine:
     """
     def convert(self, a, b, p, c, d, q):
         mat = [
-            [a, c, 0],
-            [b, d, 0],
-            [p, q, 1],
+            a, c, 0,
+            b, d, 0,
+            p, q, 1,
         ]
         self.compose(mat)
 
@@ -23,45 +24,47 @@ class Affine:
     半時計回りに90°回転
     """
     def rotate90(self):
-        mat = [[0, 1, 0], 
-               [-1, 0, 0], 
-               [0, 0, 1]]
+        mat = [0, 1, 0, 
+               -1, 0, 0, 
+               0, 0, 1]
         self.compose(mat)
 
     """
     時計回りに90°回転
     """
     def rotate90cc(self):
-        mat = [[0, -1, 0], 
-               [1, 0, 0], 
-               [0, 0, 1]]
+        mat = [0, -1, 0, 
+               1, 0, 0, 
+               0, 0, 1]
         self.compose(mat)
 
     """
     X=xを基準にflip
     """
     def flipX(self, x=0):
-        mat = [[-1, 0, x], 
-               [0, 1, 0], 
-               [0, 0, 1]]
+        mat = [-1, 0, x, 
+               0, 1, 0, 
+               0, 0, 1]
         self.compose(mat)
 
     def flipY(self, y=0):
-        mat = [[1, 0, 0], 
-               [0, -1, y], 
-               [0, 0, 1]]
+        mat = [1, 0, 0, 
+               0, -1, y, 
+               0, 0, 1]
         self.compose(mat)
 
     def compose(self, mat):
         self.affine = self.mul(self.affine, mat)
 
     def mul(self, M, N):
-        n1 = len(M)
-        n2 = len(M[0])
-        n3 = len(N[0])
-        L = [[0] * n3 for _ in range(n1)]
-        for i in range(n1):
-            for j in range(n3):
-                for k in range(n2):
-                    L[i][j] = (L[i][j] + M[i][k] * N[k][j])
+        L = [0] * 9
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    L[i*3+j] += M[i*3+k] * N[k*3+j]
         return L
+    
+if __name__ == "__main__":
+    af = Affine()
+    af.rotate90()
+    print(af.affine)
